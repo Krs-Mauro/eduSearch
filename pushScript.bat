d702549d2adc782c6f91cabb339df3b51d70039b
Mauro <krs.mauro@gmail.com>
2025-02-06 19:20:05 -0500

    tests conditional in script

--- pushScript.bat
+++ pushScript.bat
@@ -3,8 +3,12 @@ git add .
 set /p message="Enter commit message: "
 git commit -m "%message%"
 git push
-npm run build
-timeout /t 5
+call npm run build
+IF ERRORLEVEL 1 (
+    echo Build failed
+    pause
+    exit /b 1
+)
 echo Starting Firebase deployment...
 firebase deploy
 echo Firebase deployment finished.
